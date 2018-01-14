7c0e78cfb76c09ebc9e369f3387275e117968921
kruukru <jermaine.geronimo@gmail.com>
2018-01-14 21:47:00 +0800

    generic

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -13,14 +13,14 @@ class CompanySeeder extends Seeder
     public function run()
     {
         DB::table('companytbl')->insert([
-            'name' => 'AMCOR Security & Investigation Agency, Inc.',
-            'shortname' => 'AMCOR',
-            'address' => '353 Doña Dolores Building San Rafael St., Brgy. Plainview Mandaluyong City',
-            'license' => 'PSA-130548-2017',
+            'name' => 'Security Guard Agency',
+            'shortname' => 'SG',
+            'address' => 'Address',
+            'license' => 'XXX-999999-999',
             'expiration' => Carbon::today()->addYears(3),
-            'contactno' => 'Tel No. 531-2558, 531-2517 & 531-2593',
-            'email' => 'amcor_group@yahoo.com',
-            'logo' => 'amcor.png'
+            'contactno' => 'Contact',
+            'email' => 'email@email.com',
+            'logo' => 'logo.png'
         ]);
     }
 }
